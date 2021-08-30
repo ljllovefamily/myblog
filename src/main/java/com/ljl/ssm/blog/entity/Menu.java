18b8faa58ec33974b3f6d74b7a05dbc7a172b76d
ljllovefamily <1044143112@qq.com>
2021-08-30 14:44:35 +0800

    '个人技术练习用blog项目'

---
+++ src/main/java/com/ljl/ssm/blog/entity/Menu.java
@@ -0,0 +1,25 @@
+package com.ljl.ssm.blog.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author ljl
+ */
+@Data
+public class Menu implements Serializable {
+    private static final long serialVersionUID = 489914127235951698L;
+    private Integer menuId;
+
+    private String menuName;
+
+    private String menuUrl;
+
+    private Integer menuLevel;
+
+    private String menuIcon;
+
+    private Integer menuOrder;
+
+}
